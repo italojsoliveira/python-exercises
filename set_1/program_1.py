58c472ad997a2d96e509bfcb2ebc62eaa8aaa3b9
Ítalo Oliveira <italojsoliveira1@gmail.com>
2023-10-14 22:40:29 +0200

    update

---
+++ set_1/program_1.py
@@ -0,0 +1,7 @@
+# a program that reads the user name as an input and outputs a welcome message
+
+user_name = input('Write your first name: ')
+
+print('Welcome,', user_name, '!')
+
+print('Welcome, {}!'.format(user_name))
