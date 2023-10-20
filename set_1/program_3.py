be0606b138c1bccf633eb6bea1becbfe3fb3d49b
Ítalo Oliveira <italojsoliveira1@gmail.com>
2023-10-20 13:32:46 +0200

    program_3

--- set_1/program_3.py
+++ set_1/program_3.py
@@ -1,3 +1,19 @@
 
 user_input = input('Enter something: ')
 
+print('The Python primitive datatype of this value is', type(user_input))
+
+print('Does it have only space?', user_input.isspace())
+
+print('Is it a number?', user_input.isnumeric())
+
+print('Is it alphabetic?', user_input.isalpha())
+
+print('Is it alphanumeric?', user_input.isalnum())
+
+print('Is it in uppercase?', user_input.isupper())
+
+print('Is it in lowercase?', user_input.islower())
+
+print('Is it capitalized?', user_input.istitle())
+
