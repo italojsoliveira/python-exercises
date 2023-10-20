6893d321a76815d46ac24b75b58e6945c861f52e
Ítalo Oliveira <italojsoliveira1@gmail.com>
2023-10-20 13:40:16 +0200

    program_4

---
+++ set_1/program_4.py
@@ -0,0 +1,7 @@
+user_input = int(input('Enter a number: '))
+
+antecedent = user_input - 1
+
+successor = user_input + 1
+
+print('By analyzing number {}, its antecedent is {} and its successor is {}.'.format(user_input, antecedent, successor))
