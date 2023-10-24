3132ceeb08ad6dca368aca686b789e4aec15a62a
Ítalo Oliveira <italojsoliveira1@gmail.com>
2023-10-24 15:42:38 +0200

    Program 5

---
+++ set_1/program_5.py
@@ -0,0 +1,8 @@
+
+user_input = int(input('Enter a number: '))
+
+print(  'The double of {} is {}.'.format(user_input, 2 * user_input)  )
+
+print(  'The triple of {} is {}.'.format(user_input, 3 * user_input)  )
+
+print(  'The square root of {} is {:.2f}.'.format(user_input, user_input ** 0.5)  )
