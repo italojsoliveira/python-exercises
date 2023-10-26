570c0bc4f8aeb996b129156b0e79f3710ef0dcc4
Ítalo Oliveira <italojsoliveira1@gmail.com>
2023-10-26 21:03:39 +0200

    program_7

---
+++ set_1/program_7.py
@@ -0,0 +1,16 @@
+user_input_number = int(input('Enter a number to see its multiplication table: '))
+
+print('-' * 12)
+
+print('{} x 1 = {}'.format( user_input_number, user_input_number * 1 ))
+print('{} x 2 = {}'.format( user_input_number, user_input_number * 2 ))
+print('{} x 3 = {}'.format( user_input_number, user_input_number * 3 ))
+print('{} x 4 = {}'.format( user_input_number, user_input_number * 4 ))
+print('{} x 5 = {}'.format( user_input_number, user_input_number * 5 ))
+print('{} x 6 = {}'.format( user_input_number, user_input_number * 6 ))
+print('{} x 7 = {}'.format( user_input_number, user_input_number * 7 ))
+print('{} x 8 = {}'.format( user_input_number, user_input_number * 8 ))
+print('{} x 9 = {}'.format( user_input_number, user_input_number * 9 ))
+print('{} x 10 = {}'.format( user_input_number, user_input_number * 10 ))
+
+print('-' * 12)
